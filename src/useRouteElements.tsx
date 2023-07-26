39fdc6977986b96a3dafe33d8ec162ce89ef2dcd
= <=>
2023-07-26 15:50:53 +0700

    feat: lazy load component

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -1,20 +1,21 @@
 import { Navigate, Outlet, useRoutes } from 'react-router-dom'
-import ProductList from './pages/ProductList'
-import RegisterLayout from './layouts/RegisterLayout'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import MainLayout from './layouts/MainLayout'
-import { useContext } from 'react'
+import { Suspense, lazy, useContext } from 'react'
 import { AppContext } from './contexts/app.context'
 import path from './constants/path'
-import ProductDetail from './pages/ProductDetail'
-import Cart from './pages/Cart'
+import RegisterLayout from './layouts/RegisterLayout'
+import MainLayout from './layouts/MainLayout'
 import CartLayout from './layouts/CartLayout/CartLayout'
 import UserLayout from './pages/User/layouts/UserLayout'
-import Profile from './pages/User/pages/Profile'
-import ChangePassword from './pages/User/pages/ChangePassword'
-import HistoryPurchase from './pages/User/pages/HistoryPurchase'
-import NotFound from './pages/NotFound'
+
+const Login = lazy(() => import('./pages/Login'))
+const Register = lazy(() => import('./pages/Register'))
+const ProductDetail = lazy(() => import('./pages/ProductDetail'))
+const ProductList = lazy(() => import('./pages/ProductList'))
+const Cart = lazy(() => import('./pages/Cart'))
+const Profile = lazy(() => import('./pages//User/pages/Profile'))
+const ChangePassword = lazy(() => import('./pages/User/pages/ChangePassword'))
+const HistoryPurchase = lazy(() => import('./pages/User/layouts/UserLayout'))
+const NotFound = lazy(() => import('./pages/NotFound'))
 
 const ProtectedRoute = () => {
   const { isAuthenticated } = useContext(AppContext)
@@ -36,7 +37,9 @@ const useRouteElements = () => {
           path: path.login,
           element: (
             <RegisterLayout>
-              <Login />
+              <Suspense>
+                <Login />
+              </Suspense>
             </RegisterLayout>
           )
         },
@@ -44,7 +47,9 @@ const useRouteElements = () => {
           path: path.register,
           element: (
             <RegisterLayout>
-              <Register />
+              <Suspense>
+                <Register />
+              </Suspense>
             </RegisterLayout>
           )
         }
@@ -55,7 +60,9 @@ const useRouteElements = () => {
       index: true,
       element: (
         <MainLayout>
-          <ProductList />
+          <Suspense>
+            <ProductList />
+          </Suspense>
         </MainLayout>
       )
     },
@@ -64,7 +71,9 @@ const useRouteElements = () => {
       index: true,
       element: (
         <MainLayout>
-          <ProductDetail />
+          <Suspense>
+            <ProductDetail />
+          </Suspense>
         </MainLayout>
       )
     },
@@ -76,7 +85,9 @@ const useRouteElements = () => {
           path: path.cart,
           element: (
             <CartLayout>
-              <Cart />
+              <Suspense>
+                <Cart />
+              </Suspense>
             </CartLayout>
           )
         },
@@ -90,15 +101,27 @@ const useRouteElements = () => {
           children: [
             {
               path: path.profile,
-              element: <Profile />
+              element: (
+                <Suspense>
+                  <Profile />
+                </Suspense>
+              )
             },
             {
               path: path.changePassword,
-              element: <ChangePassword />
+              element: (
+                <Suspense>
+                  <ChangePassword />
+                </Suspense>
+              )
             },
             {
               path: path.historyPurchase,
-              element: <HistoryPurchase />
+              element: (
+                <Suspense>
+                  <HistoryPurchase />
+                </Suspense>
+              )
             }
           ]
         }
@@ -108,7 +131,9 @@ const useRouteElements = () => {
       path: '*',
       element: (
         <MainLayout>
-          <NotFound />
+          <Suspense>
+            <NotFound />
+          </Suspense>
         </MainLayout>
       )
     }
